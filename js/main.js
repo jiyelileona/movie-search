7abf6c5c0983f5fa383e53fd5fd089cc16fb97c7
Jiye Li <15832778446@163.com>
2021-01-04 10:16:45 -0600

    refactor the structure

--- js/main.js
+++ js/main.js
@@ -1,63 +1,33 @@
-const getMoviesData = movieName => {
-  fetch(`http://www.omdbapi.com/?apikey=f95d62f3&s=${movieName}`)
-    .then(response => {
-      if (response.status !== 200) {
-        console.log("There's an error. Status code: " + response.status);
-        return;
-      }
-      response
-        .json()
-        .then(moviesData => {
-          console.log(moviesData.Search);
-          let movieIDs = [];
-          for (let movieData of moviesData.Search) {
-            movieIDs.push(movieData.imdbID);
-          }
-          return movieIDs;
-        })
-        .then(movieIDs => {
-          if (movieIDs.length > 1) {
-            for (let movieID of movieIDs) {
-              getInfoOfMovie(movieID);
-            }
-          } else if (movieIDs == 1) {
-            getInfoOfMovie(movieIDs);
-          }
-        });
-    })
-    .catch(err => console.log('Error: ' + err));
+import regeneratorRuntime from 'regenerator-runtime';
+require('dotenv').config();
+
+const getMoviesData = async movieName => {
+  try {
+    await fetch(`https://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${movieName}`)
+      .json()
+      .Search.map(search => search.imdbID)
+      .map(id => getInfoOfMovie(id));
+  } catch (err) {
+    return "There's an error: " + err.message;
+  }
 };
 
-const getInfoOfMovie = id => {
-  fetch(`http://www.omdbapi.com/?apikey=f95d62f3&i=${id}`).then(response => {
-    if (response.status !== 200) {
-      console.log("There's an error. Status code: " + response.status);
-      return;
-    }
-
-    response.json().then(data => {
-      let info = {
-        title: data.Title,
-        rating: data.imdbRating,
-        plot: data.Plot,
-        poster: data.Poster,
-      };
-      createHTML(info);
-    });
-  });
+const getInfoOfMovie = async id => {
+  const movieInfo = await fetch(`https://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${id}`);
+  let {Title, imdbRating, Plot, Poster} = await movieInfo.json();
+  createHTML(Title, imdbRating, Plot, Poster);
 };
 
 const movies = document.querySelector('.movies');
-const movie = document.querySelectorAll('.movie');
 
-const createHTML = info => {
+const createHTML = (Title, imdbRating, Plot, Poster) => {
   movies.insertAdjacentHTML(
     'beforeend',
-    `<div class="movie" style="background-image: url(${info.poster})">
-          <p>${info.title}</p>
-          <p>${info.rating}</p>
-          <p>${info.plot}</p>
-       </div>
+    `<div class="movie" style="background-image: url(${Poster})">
+      <p>${Title}</p>
+      <p>${imdbRating}</p>
+      <p>${Plot}</p>
+    </div>
       `
   );
 };
@@ -88,14 +58,13 @@ searchBar.addEventListener('keypress', e => {
     movies.innerHTML = '';
     getMoviesData(searchBar.value);
     searchBar.value = '';
-    searchBar.blur()
+    searchBar.blur();
   }
 });
 
 let searchBox = document.querySelector('.searchBar');
 
 searchBox.addEventListener('click', e => {
-  console.log(e.target)
   if (e.target.tagName == 'DIV') {
     searchBar.value = '';
   }
